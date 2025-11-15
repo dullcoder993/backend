3af5e2862d77115921e8131eb87208c92aa36e61
dullcoder993 <prathmeshkulkarni9529@gmail.com>
2025-11-15 18:05:44 +0530

    Handling Api ApiError

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import dotenv from "dotenv"
-
-
-dotenv.config()
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
+import { app } from "./app.js";
+
+dotenv.config()
+
 
 
 const connectDB = async () => {
@@ -20,6 +21,20 @@ const connectDB = async () => {
 
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log('ERRR : ',error);
+        throw error
+    })
+    
+    
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed ",err);
+})
 
 
 
