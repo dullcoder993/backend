5f831916c2c78c7fda55890427d91c95ea5354b1
dullcoder993 <prathmeshkulkarni9529@gmail.com>
2025-11-16 15:01:42 +0530

    ADD USER AND VIDEO MODEL

---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema({
+    vidoFile: {
+        type: String,
+        required: true
+    },
+    thumbnail:{
+        type: String,
+        required:true,
+    },
+    title:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    duration:{
+        type :Number, 
+        required: true
+    },
+    views : {
+        type : Number,
+        default:0,
+        required:true
+    },
+    isPublished:{
+        type : Boolean,
+        default:true,
+        required:true
+    },
+    owner:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref:'user'
+    }
+},{timestamps:true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const video = mongoose.model('video',videoSchema)
